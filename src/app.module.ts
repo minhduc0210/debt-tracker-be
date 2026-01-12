c47ac7ff6f58e75f8e6f5b1a76168d3d5f9886c8
Enzo Nguyen <haquocminhduc@gmail.com>
2026-01-12 08:35:32 +0700

    Update: migrate from sqlite to postgresql

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,7 +8,7 @@ import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
+    ConfigModule.forRoot({ isGlobal: true, envFilePath: '.env' }),
     PrismaModule,
     ContactsModule,
     TransactionsModule,
--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,11 +1,27 @@
-import { Injectable } from '@nestjs/common';
-import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3';
+import 'dotenv/config';
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from 'generated/prisma/client';
 
 @Injectable()
-export class PrismaService extends PrismaClient {
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
   constructor() {
-    const adapter = new PrismaBetterSqlite3({ url: process.env.DATABASE_URL });
+    const adapter = new PrismaPg({
+      connectionString: process.env.DATABASE_URL,
+    });
+    console.log('Prisma adapter created');
     super({ adapter });
+  }
+
+  async onModuleInit() {
+    await this.$connect();
+    console.log('Prisma connected');
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
   }
 }
